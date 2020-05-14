7c8e5d5dcdc2c953ef4a5ae8ab42fc38a61cc7a8
Hideki Takano <hideki.takano@uhuru.jp>
2020-05-14 20:12:37 +0900

    modify for creating packages #1334

--- scripts/generate-publish-script.js
+++ scripts/generate-publish-script.js
@@ -5,34 +5,34 @@ const fs = require("fs-extra");
 const should = require("should");
 
 function generateScript() {
-    return new Promise((resolve, reject) => {
-        const packages = [
-            "node-red-util",
-            "node-red-runtime",
-            "node-red-registry",
-            "node-red-nodes",
-            "node-red-editor-client",
-            "node-red-editor-api",
-            "node-red"
-        ];
-        const rootPackage = require(path.join(__dirname,"..","package.json"));
-        const version = rootPackage.version;
+  return new Promise((resolve, reject) => {
+    const packages = [
+      "enebular-node-red-util",
+      "enebular-node-red-runtime",
+      "enebular-node-red-registry",
+      "enebular-node-red-nodes",
+      "enebular-node-red-editor-client",
+      "enebular-node-red-editor-api",
+      "enebular-node-red",
+    ];
+    const rootPackage = require(path.join(__dirname, "..", "package.json"));
+    const version = rootPackage.version;
 
-        const tagArg = /-/.test(version) ? "--tag next" : ""
+    const tagArg = /-/.test(version) ? "--tag next" : "";
 
-        const lines = [];
+    const lines = [];
 
-        packages.forEach(name => {
-            lines.push(`npm publish ${name}-${version}.tgz ${tagArg}\n`);
-        })
-        resolve(lines.join(""))
+    packages.forEach((name) => {
+      lines.push(`npm publish ${name}-${version}.tgz ${tagArg}\n`);
     });
+    resolve(lines.join(""));
+  });
 }
 
 if (require.main === module) {
-    generateScript().then(output => {
-        console.log(output);
-    });
+  generateScript().then((output) => {
+    console.log(output);
+  });
 } else {
-    module.exports = generateScript;
+  module.exports = generateScript;
 }
